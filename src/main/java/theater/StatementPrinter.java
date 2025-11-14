a14adca129a317f7095d27a32961b3d177aa32e0
waves <wavesj04@gmail.com>
2025-11-14 18:57:55 -0500

    Task 1.1

--- src/main/java/theater/StatementPrinter.java
+++ src/main/java/theater/StatementPrinter.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 /**
  * This class generates a statement for a given invoice of performances.
+ * Completed 1.1.
  */
 public class StatementPrinter {
     private final Invoice invoice;
